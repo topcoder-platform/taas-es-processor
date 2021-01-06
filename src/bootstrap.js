ffc22bd868545b874adcbfd5a8a9db7aaca7fb9a
imcaizheng <imcaizheng@gmail.com>
2021-01-06 15:45:45 +0800

    add title field to Job model
    - update ES mapping for the Job index
    - update Joi schema

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,6 +8,7 @@ Joi.rateType = () => Joi.string().valid('hourly', 'daily', 'weekly', 'monthly')
 Joi.jobStatus = () => Joi.string().valid('sourcing', 'in-review', 'assigned', 'closed', 'cancelled')
 Joi.jobCandidateStatus = () => Joi.string().valid('open', 'selected', 'shortlist', 'rejected', 'cancelled')
 Joi.workload = () => Joi.string().valid('full-time', 'fractional')
+Joi.title = () => Joi.string().max(64)
 
 const zapierSwitch = Joi.string().label('ZAPIER_SWITCH').valid(...Object.values(constants.Zapier.Switch))
 
