8f03a1ab84b8034102f3b72eccf458e949480c04
maxceem <maxceem@gmail.com>
2021-01-15 11:03:46 +0200

    feat: make job title longer (128)

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -8,7 +8,7 @@ Joi.rateType = () => Joi.string().valid('hourly', 'daily', 'weekly', 'monthly')
 Joi.jobStatus = () => Joi.string().valid('sourcing', 'in-review', 'assigned', 'closed', 'cancelled')
 Joi.jobCandidateStatus = () => Joi.string().valid('open', 'selected', 'shortlist', 'rejected', 'cancelled')
 Joi.workload = () => Joi.string().valid('full-time', 'fractional')
-Joi.title = () => Joi.string().max(64)
+Joi.title = () => Joi.string().max(128)
 
 const zapierSwitch = Joi.string().label('ZAPIER_SWITCH').valid(...Object.values(constants.Zapier.Switch))
 
